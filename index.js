62126d2f181d95f28f648ef26ada04aeaf9f24d0
Søren Sønderby Nielsen <ssni@itu.dk>
2017-02-06 20:47:37 +0100

    Slack lunch slash command

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 var lunch = require('./lunch.js')
 var botkit = require('botkit')
+var bodyParser = require('body-parser')
+var request = require('request')
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
 	lunch.today(function (lunch, err) {
@@ -43,3 +47,34 @@ controller.hears('lunch', ['direct_message','direct_mention','mention'], functio
 		bot.reply(message, lunch.main + " | " + lunch.side);
 	})
 });
+
+
+app.post('/slack/slash/lunch', function (req, res) {
+	console.log(req.body)
+
+	if (req.body['token'] != process.env.SLACK_SLASH_LUNCH) {
+		res.send()
+		return
+	}
+
+	lunch.today(function (lunch, err) {
+		if (err) {
+			console.log("Error: " + err)
+			return
+		}
+
+		request({
+			method: 'POST',
+			uri: req.body['response_url'],
+			json: {
+				response_type: "in_channel",
+				text: lunch.main,
+				attachments: [{
+					text: lunch.side
+				}]
+			}
+		})
+	})
+
+	res.send()
+})
