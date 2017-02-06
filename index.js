acf347c3d96db90c40160b896549031d45ea89bd
Søren Sønderby Nielsen <ssn@e-conomic.com>
2017-02-06 18:05:15 +0100

    Slack bot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var lunch = require('./lunch.js')
+var botkit = require('botkit')
 
 app.get('/', function (req, res) {
 	lunch.today(function (lunch, err) {
@@ -18,3 +19,27 @@ app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), function() {
 	console.log("Running on port " + app.get('port'))
 })
+
+var controller = botkit.slackbot({
+	debug: false
+	//include "log: false" to disable logging
+	//or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+	token: 'xoxb-136922031585-9HuIoAyzSjqg6TEiIykQvS5z',
+}).startRTM()
+
+// give the bot something to listen for.
+controller.hears('lunch',['direct_message','direct_mention','mention'], function(bot,message) {
+	lunch.today(function (lunch, err) {
+		if (err) {
+			console.log("Error: " + err)
+			bot.reply(message, "Could not grap lunch info :/")
+			return
+		}
+
+		bot.reply(message, lunch.main + " | " + lunch.side);
+	})
+});
