7abb831aab50aad7f7489e2de320ab1b364a3307
Søren Sønderby Nielsen <ssn@e-conomic.com>
2017-02-07 16:33:02 +0100

    Fix week number bug

--- lunch.js
+++ lunch.js
@@ -1,31 +1,30 @@
 var request = require('request')
 var cheerio = require('cheerio')
+var zpad = require('zpad')
+var currentWeekNumber = require('current-week-number')
 
-function mainQuery(weekdayNumber) {
-	return "div#w06-" + weekdayNumber + " .menu-row .row .title span"
+function mainQuery(weekNumber, weekdayNumber) {
+	return "div#" + zpad(weekNumber) + "-" + weekdayNumber + " .menu-row .row .title span"
 }
 
-function sideQuery(weekdayNumber) {
-	return "div#w06-" + weekdayNumber + " .menu-row .row .description span"
-}
-
-function getWeekdayNumber() {
-	var date = new Date()
-	return date.getDay() - 1
+function sideQuery(weekNumber, weekdayNumber) {
+	return "div#" + zpad(weekNumber) + "-" + weekdayNumber + " .menu-row .row .description span"
 }
 
 var lastFetchedForWeekdayNumber
 var cachedLunch
 
 function today(cb) {
-	var weekdayNumber = getWeekdayNumber()
+	var date = new Date()
+	var weekdayNumber = date.getDay() - 1
+	var weekNumber = currentWeekNumber()
+
 	if (lastFetchedForWeekdayNumber != weekdayNumber) {
 		request('http://dk428.eurest.dk/k_benhavn/ugemenu', function (err, res, body) {
 			var $ = cheerio.load(body)
-			var weekdayNumber = getWeekdayNumber()
 			var lunch = {
-				main: $(mainQuery(weekdayNumber)).text().trim(),
-				side: $(sideQuery(weekdayNumber)).text().trim()
+				main: $(mainQuery(weekNumber, weekdayNumber)).text().trim(),
+				side: $(sideQuery(weekNumber, weekdayNumber)).text().trim()
 			}
 			lastFetchedForWeekdayNumber = weekdayNumber
 			cachedLunch = lunch
